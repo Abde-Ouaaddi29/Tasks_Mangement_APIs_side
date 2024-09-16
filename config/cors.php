8378a8dedede76596c2d293c5336470624112cba
Abde-Ouaaddi29 <abderrahimouaaddi371@gmail.com>
2024-09-16 01:51:55 +0100

    second commit

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://tasks-management-ouaaddi.netlify.app', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
